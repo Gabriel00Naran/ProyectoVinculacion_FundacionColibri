a3b02a224e2204b92d87646ffb5b4e10f71c5599
joseobando0001 <jose.obando_0001@hotmail.com>
2021-05-11 23:12:53 -0500

    Cambios en el HTML
    Cambios en la visualizacion del login y adicion de spinners y alertas

--- Client-Plantilla/src/app/pages/dashboard/dashboard.component.ts
+++ Client-Plantilla/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
 
-  constructor() { }
+  constructor(private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
+    this.spinner.show();
+
+    setTimeout(() => {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Inicio de Sesión Satisfactorio'
+      })
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 3000);
 
   }
 
